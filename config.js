6ed5daf1a596d62cd30897bdcabbb454ff7ec649
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-05-23 12:38:49 -0400

    Change Realtor Details

--- config.js
+++ config.js
@@ -1,80 +1,54 @@
-const config = {
+{
   "property": {
-    "streetAddress": "142 Valley Glen Heights NW",
-    "city": "Calgary",
-    "cityStateZip": "Calgary, AB T3B 5S8",
-    "heroSubtitle": "Amazing back yard, expansive 44x146-foot lot (7,588 sq ft) backing onto a tranquil nature reserve and the renowned Bow River Pathway. Register to receive the complete property Home &amp; Neighbourhood Info Package, virtual tour, floor plans, and comparables.",
-    "backgroundImageUrl": "https://cdn.realtor.ca/listing/TS638829328681570000/reb9/highres/1/A2219911_1.jpg"
+    "streetAddress": "80 John St #2912",
+    "city": "Toronto",
+    "cityStateZip": "Toronto, ON M5V 3X4",
+    "heroSubtitle": "1249 SqFt split-bedroom corner condo in The Iconic Entertainment District. Enjoy unbeatable city views, upgraded kitchen & bathrooms, and direct access to Michelin-starred restaurants, designer boutiques, transit, and Toronto's PATH & Financial District. Register to receive the complete Open House Info Package, virtual tour, floor plan, and comparables!",
+    "backgroundImageUrl": "https://api.typeform.com/responses/files/fb1b1f0e23cd904d0766d77e941c1269ac1a1421a9f806f930245d5bbdeafa82/2912_80_John_St_01.jpg"
   },
   "realtor": {
-    "fullName": "Amy Caughlin",
+    "fullName": "Arslan Ahmed",
     "title": "REALTOR®",
-    "phone": "15874391618",
+    "phone": "4166554850",
     "email": "arslan@salesgenius.co",
-    "bio": "A former risk manager turned Realtor, proptech co-founder, podcast host, and passionate community advocate. I help busy professionals and families find homes that truly fit their lives, while working behind the scenes to make real estate better for everyone involved. With a background in risk management, I bring a strategic lens to every transaction, ensuring my clients make informed, confident decisions. I’m also the co-founder of Jazzberry, a platform designed by realtors, for realtors. A proud Maritimer, I lead with authenticity, hospitality, and heart. Whether I’m running along the Bow River, adventuring in the mountains with my family, or hosting FutureRE — The Future of Real Estate, I’m always driven by curiosity, connection, and impact.",
-    "photoUrl": "https://static.showit.co/800/TUEDlhwVYAO5v1iU87fE1w/264146/headshot.jpg",
+    "bio": "If you're looking to rent a condo in Toronto, look no further than Andrew Ahmed with Condos.ca. With several years of experience navigating the condo industry, I'll provide you with guidance as you look for a home. Give me a call and get started today!",
+    "photoUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/73482f1e-fecf-4dae-8733-27b38a97f827/headshot_no_bg.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB4666AMJZ6ZW%2F20250523%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250523T163751Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDgaCXVzLXdlc3QtMiJGMEQCIHeFtULffh4inBDyzL5Pl%2B9uDeahWOzKklLSKwjo36aXAiA4gZUg3YxoApYfeWpnGvE5tvAWlxHDWQM82aCex74utiqIBAjx%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAAaDDYzNzQyMzE4MzgwNSIMQD282i8H2cRDDb2UKtwDux8XTpKp9rAS81%2BsMzhC0nO7tbRvhCcAobL89t2A2MRnmJ4MI6ng%2BnSF1d19H4EtCLXZj1WAcFA9EwYrt2SIe%2B4q0Jd5Xf2fP20nSnUqfPEe5Ft5%2FubE8Lmx2U7SDnJGqG%2FfgKgA%2BZlymUKCjhkhqO1KFbY5Zg%2BKEkQ0FXWmP4K9zx9B2Ynn9sOKwxaF1ddNtWtQ2z5bzgnBb3fIpA646xk8oPPBNIF47wvayZG4Ov6NxhurBpFCQ1H%2FnTCWktHEQ58TBn%2FUdBa3vx%2FSBNPUuMkU4vMHC3r0Z3wG%2BxUn9hGMnY8V7m7ufJKoNjAeS5ZBFqZ0YUDhF5fLx7C7nvsvIhfFgnwzrJw5xY8IsoHFlZTLMetj13GbvG6YzMRSpQXH%2FhZa%2BxYUXLiXmArZn3VcdTkKl4szNCCNEuEl69Q16zBBfIX6z81wk0nqnPZJzSaSHxGxArsfHKi8BNH3gpLxdyVzVXTcCCqTxd4E03XQb6DezeQDiFhnuKEyicVWnmmmyBCyAra7dj2TfoqK2iujVoEn7ZVl%2BV696SMvRfbQ1XgpRJDpPAmGc4LU4czdMUNWEevO%2FQwq9K3Y3qM6bc%2BEs9SH6rRY0VYWFOl3KAlrdP%2FfrQwt6SPbK1G97pkw177CwQY6pgEtQ6GbOcURx8AvC8Pk4JaKwDSed3%2BiSkWFzmAi6Vww0Gz3TFa4b7DhCQF6sbpSi5gLGgxhuQIcpolQaHKreB8%2FSiG2tHHjjMwfj383WRPNKi1quOSDzFTnbHCdUBjsyJcphn6RDiSDhQIyhcHfTQlRknD14Viv%2FQW2xKQ1CjZO7%2B5RZAtmoCDX31a%2FVSNfT645cih%2BF23we3MdXriwWqHRhCwOpExb&X-Amz-Signature=f740715762c485c8c0215664c49d45beb48576578e64c440f660b7eb3cf46780&X-Amz-SignedHeaders=host&x-id=GetObject",
     "social": {
-      "facebook": "https://www.facebook.com/caughlinrealestate/",
-      "instagram": "https://www.instagram.com/caughlin.realestate/",
-      "website": "https://caughlinrealestate.ca/"
+      "facebook": "https://www.facebook.com/Andrewabroker/",
+      "instagram": null,
+      "website": null
     }
   },
   "brokerage": {
-    "name": "Greater Property Group",
-    "logoUrl": "https://static.showit.co/400/I76PcfBRdfuEObejOPwzPQ/264146/gpg-no-tagline-logo-white.png",
-    "address": "5940 Macleod Trl SW #500, Calgary, AB T2H 2G4"
+    "name": "EXECUTIVE HOMES REALTY INC.",
+    "logoUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/3780665a-d3ac-437b-b4a0-95065664fc6a/Office_Logo.png",
+    "address": "290 TRADERS BLVD EAST #1 MISSISSAUGA, Ontario L4Z1W7"
   },
   "openHouse": {
-    "eventDate": "2025-05-17",
+    "eventDate": "2025-05-24",
+    "secondEventDate": "2025-05-25",
     "bundleItems": [
-      {
-        "icon": "fas fa-ruler-combined",
-        "text": "Architectural Floor Plan"
-      },
-      {
-        "icon": "fas fa-star",
-        "text": "Neighbourhood Guide"
-      },
-      {
-        "icon": "fas fa-check-circle",
-        "text": "Market Stats"
-      },
-      {
-        "icon": "fas fa-car",
-        "text": "School District Information"
-      },
-      {
-        "icon": "fas fa-key",
-        "text": "Financing Resources"
-      },
-      {
-        "icon": "fas fa-home",
-        "text": "Utility Cost Estimates"
-      },
-      {
-        "icon": "fas fa-map-location-dot",
-        "text": "Property Feature List"
-      },
-      {
-        "icon": "fas fa-image",
-        "text": "Home Inspection Tips"
-      }
+      { "icon": "fas fa-ruler-combined", "text": "Floor Plan", "url": "https://cdn.realtor.ca/listing/TS638834353319130000/reb82/highres/6/c12162916_3.jpg" },
+      { "icon": "fas fa-link", "text": "Property Listing URL", "url": "https://www.realtor.ca/real-estate/28344175/2912-80-john-street-toronto-waterfront-communities-waterfront-communities-c1" },
+      { "icon": "fas fa-image", "text": "Photos", "url": "https://tours.openhousemedia.ca/sites/80-john-st-2912-toronto-on-m5v-3x4-15850296/branded" },
+      { "icon": "fas fa-video", "text": "Virtual Tour", "url": "https://my.matterport.com/show/?m=zcSrKCKA41k&lang=en&play=1" },
+      { "icon": "fas fa-chart-line", "text": "Recent Neighborhood Sales", "url": "https://www.realtor.ca/real-estate/28344175/2912-80-john-street-toronto-waterfront-communities-waterfront-communities-c1" }
     ]
   },
   "meta": {
-    "pageTitlePrefix": "🏡 Amy Caughlin Open House",
+    "pageTitlePrefix": "🏡 Arslan Ahmed Open House",
     "navBrandLogoText": "OPEN HOUSE",
-    "privacyPolicyLink": "https://caughlinrealestate.ca/"
+    "privacyPolicyLink": "https://www.executivehomesrealty.ca/privacy-policy"
   },
   "settings": {
     "adminWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouseupdate",
     "visitorWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouse",
     "confettiColors": [
-      "#6187a7",
-      "#f0e9d6",
-      "#ffffff",
-      "#83a363",
-      "#d9ac68"
+      "#233552",
+      "#e2dee3",
+      "#aecbe4",
+      "#7fd5bc",
+      "#f7c873"
     ]
   },
   "modalQuestions": [
@@ -116,10 +90,9 @@ const config = {
     }
   ],
   "deploymentInfo": {
-    "repoName": "Open-House-957",
-    "repoUrl": "https://github.com/arslvn93/Open-House-957",
-    "tag": "Open House 142 Valley Glen Heights NW",
-    "netlifyUrl": "https://services.leadconnectorhq.com/hooks/zpjvSoJ3TMkdq8pcj7VJ/webhook-trigger/GIJI1X6L1zS4PxcScJqY",
-    "netlifyId": "dc35cadf-51e9-4ca8-843c-a084dd57add1"
+    "repoName": "Open-House-926",
+    "repoUrl": "https://github.com/arslvn93/Open-House-926",
+    "tag": "Open House 80 John St #2912 Toronto ON",
+    "siteId": "989155550"
   }
-};
+}
